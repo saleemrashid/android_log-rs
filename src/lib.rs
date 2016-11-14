6246cefb2a8afa4d08b840a9cc7d2c19afafb142
Saleem Rashid <dev@saleemrashid.com>
2016-11-14 18:57:42 +0000

    AndroidLogger: Create documented stub
    TODO: Actually log stuff

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@
 //! extern crate android_log;
 //!
 //! fn main() {
-//!     android_log::init("MyApp");
+//!     android_log::init("MyApp").unwrap();
 //!
 //!     trace!("Initialized Rust");
 //!     debug!("Address is {:p}", main as *const ());
@@ -25,3 +25,119 @@
 //! 12-25 12:00:00.000  1234  1234 I MyApp: Did you know? 1 + 1 = 2
 //! 12-25 12:00:00.000  1234  1234 W MyApp: Don't log sensitive information!
 //! 12-25 12:00:00.000  1234  1234 E MyApp: Nothing more to say
+
+extern crate log;
+
+use log::{Log, LogLevelFilter, LogMetadata, LogRecord, SetLoggerError};
+
+/// `AndroidLogger` is the implementation of the logger.
+///
+/// It should not be used from Rust libraries which should only use the facade.
+pub struct AndroidLogger {
+    tag: String,
+    format: Box<Fn(&LogRecord) -> String + Sync + Send>,
+}
+
+/// `LogBuilder` acts as builder for initializing the `AndroidLogger`. It can be
+/// used to customize the log format.
+///
+/// ## Example
+///
+/// ```
+/// #[macro_use] extern crate log;
+/// extern crate android_log;
+///
+/// use log::{LogRecord, LogLevelFilter};
+/// use android_log::LogBuilder;
+///
+/// fn main() {
+///     let format = |record: &LogRecord| {
+///         format!("{} - {}", record.target(), record.args())
+///     };
+///
+///     let mut builder = LogBuilder::new();
+///     builder.format(format);
+///
+///     builder.init().unwrap();
+///
+/// 	warn!("Warning message");
+///     error!("Error message");
+/// }
+/// ```
+pub struct LogBuilder {
+    tag: String,
+    format: Box<Fn(&LogRecord) -> String + Sync + Send>,
+}
+
+/// Initializes the global logger with an `AndroidLogger`
+///
+/// This should be called early in the execution of a Rust program and the
+/// global logger may only be initialized once. Future attempts will return an
+/// error.
+pub fn init<S: Into<String>>(tag: S) -> Result<(), SetLoggerError> {
+    AndroidLogger::new(tag).init()
+}
+
+impl AndroidLogger {
+    /// Initializes the logger with defaults
+    pub fn new<S: Into<String>>(tag: S) -> AndroidLogger {
+        LogBuilder::new(tag).build()
+    }
+
+    /// Initializes the global logger with `self`
+    pub fn init(self) -> Result<(), SetLoggerError> {
+        log::set_logger(|max_level| {
+            max_level.set(LogLevelFilter::Off);
+            Box::new(self)
+        })
+    }
+}
+
+impl Log for AndroidLogger {
+    fn enabled(&self, _: &LogMetadata) -> bool {
+        true
+    }
+
+    fn log(&self, record: &LogRecord) {
+        if !Log::enabled(self, record.metadata()) {
+            return;
+        }
+
+        // TODO: Implement logging
+    }
+}
+
+impl LogBuilder {
+    /// Initializes the builder with defaults
+    pub fn new<S: Into<String>>(tag: S) -> LogBuilder {
+        LogBuilder {
+            tag: tag.into(),
+            format: Box::new(|record: &LogRecord| {
+                format!("{}: {}",
+                        record.location().module_path(),
+                        record.args())
+            }),
+        }
+    }
+
+    /// Sets the format function for formatting the log output
+    pub fn format<F: 'static>(&mut self, format: F) -> &mut Self
+        where F: Fn(&LogRecord) -> String + Sync + Send
+    {
+        self.format = Box::new(format);
+        self
+    }
+
+    /// Builds an `AndroidLogger` and initializes the global logger
+    pub fn init(self) -> Result<(), SetLoggerError> {
+        self.build().init()
+    }
+
+    /// Builds an `AndroidLogger`
+    pub fn build(self) -> AndroidLogger {
+        AndroidLogger {
+            tag: self.tag,
+            format: self.format,
+        }
+    }
+}
